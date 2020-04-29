574f30c198420102d814831b49cbf4c5988f0189
Rose Liverman <paul.liverman.iii@gmail.com>
2020-04-29 04:41:52 -0700

    improved testing docker config

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e # exit on any failure
-
-moonc .
-sudo docker build -t guard13007/tangentfox-com:testing .
-sudo docker push guard13007/tangentfox-com:testing
-./test.sh
